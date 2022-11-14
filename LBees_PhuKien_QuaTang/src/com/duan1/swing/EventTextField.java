420f7f3f2cb40e1c8cd4d7a6e86ad6057e17b32a
phungtuhieu2015 <phungtuhieu2015@gmail.com>
2022-11-14 09:00:04 +0700

    Tu Hieu - Gop nhanh huuTai

---
+++ LBees_PhuKien_QuaTang/src/com/duan1/swing/EventTextField.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.duan1.swing;
+
+/**
+ *
+ * @author MSI PC
+ */
+public interface EventTextField {
+    public void onPressed(EventCallBack call);
+
+    public void onCancel();
+}
