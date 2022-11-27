052d081aca0849cd437154ce3495e51395b9eb64
phungtuhieu2015 <phungtuhieu2015@gmail.com>
2022-11-27 13:35:29 +0700

    Tu Hieu - Tao package Helper

---
+++ LBees_PhuKien_QuaTang/src/com/duan1/Helper/XJdbc.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.duan1.Helper;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author vanvi
+ */
+public class XJdbc {
+     private static String driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+    private static String dburl = "jdbc:sqlserver://localhost:1433;databaseName=QL_PHU_KIEN_QUA_TANG5;encrypt=true;trustServerCertificate=true";
+    private static String username = "sa";
+    private static String password = "123";
+    static {
+        try {
+            Class.forName(driver);
+        } catch (ClassNotFoundException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+    public static PreparedStatement prepareStatement(String sql, Object... args) throws SQLException {
+        Connection connection = DriverManager.getConnection(dburl, username, password);
+        PreparedStatement pstmt = null;
+        if (sql.trim().startsWith("{")) {
+            pstmt = connection.prepareCall(sql);
+        } else {
+            pstmt = connection.prepareStatement(sql);
+        }
+        for (int i = 0; i < args.length; i++) {
+            pstmt.setObject(i + 1, args[i]);
+        }
+        return pstmt;
+    }
+     public static void executeUpdate(String sql, Object... args) {
+        try {
+            PreparedStatement stmt = prepareStatement(sql, args);
+            try {
+                stmt.executeUpdate();
+            } finally {
+                stmt.getConnection().close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+     public static ResultSet executeQuery(String sql, Object... args) {
+        try {
+            PreparedStatement stmt = prepareStatement(sql, args);
+            return stmt.executeQuery();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+   
+}
